6857cdc1bb3728349d45d1ab66bedd61cac9884a
Igor Kodzhebash <igorkodzebash@gmail.com>
2023-04-25 20:57:34 +0300

    feat: implement function to create each key

--- js/script.js
+++ js/script.js
@@ -18,16 +18,52 @@ function createKeyboard() {
   container.append(keyboard)
   createKeyboardRow(data, keyboard)
 
+  const keyboardRows = document.querySelectorAll('.keyboard-row');
+
+  for (let i = 0; i < data.length; i++) {
+    createKeyboardKeys(data[i], keyboardRows[i])
+  }
 }
 
 createKeyboard()
+
+
+// function create keyboard rows for keyboard
 
 function createKeyboardRow(array, parenElement) {
   array.forEach(el => {
     const keyboardRow = document.createElement('div');
     keyboardRow.classList.add('keyboard-row');
     parenElement.append(keyboardRow);
-
+    // createKeyboardKeys(data, keyboardRow)
   });
-
 }
+
+
+// function create each key for keyboard
+
+function createKeyboardKeys(array, parentRow) {
+
+    for (const subEl of array) {
+
+      const key = document.createElement('div')
+      key.innerHTML = `
+      <div class="keyboard-key ${subEl.addClass}">
+          <span class="eng hidden">
+            <span class="case-down hidden">${subEl.caseDown}</span>
+            <span class="case-up hidden">${subEl.ukr.caseUp}</span>
+            <span class="case-caps hidden">${subEl.ukr.caseCaps}</span>
+            <span class="case-capsShift hidden">${subEl.ukr.caseCapsShift}</span>
+          </span>
+          <span class="ukr">
+            <span class="case-down">${subEl.ukr.caseDown}</span>
+            <span class="case-up hidden">${subEl.ukr.caseUp}</span>
+            <span class="case-caps hidden">${subEl.ukr.caseCaps}</span>
+            <span class="case-capsShift hidden">${subEl.ukr.caseCapsShift}</span>
+          </span>
+        </div>
+      `
+      parentRow.append(key)
+    }
+  }
+
