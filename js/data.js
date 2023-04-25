1936a444d48a3d8a28a2b5947c2989618920b67f
Igor Kodzhebash <igorkodzebash@gmail.com>
2023-04-25 19:56:36 +0300

    feat: prepare data for the first row

--- js/data.js
+++ js/data.js
@@ -15,7 +15,216 @@ const data = [
         caseCapsShift: '₴'
       },
       addClass: 'backquote'
-    }
+    },
+    {
+      name: 'digit1',
+      eng: {
+        caseDown: '1',
+        caseUp: '!',
+        caseCaps: '1',
+        caseCapsShift: '!'
+      },
+      ukr: {
+        caseDown: "1",
+        caseUp: '!',
+        caseCaps: "1",
+        caseCapsShift: '!'
+      },
+      addClass: 'digit1'
+    },
+    {
+      name: 'digit2',
+      eng: {
+        caseDown: '2',
+        caseUp: '@',
+        caseCaps: '2',
+        caseCapsShift: '@'
+      },
+      ukr: {
+        caseDown: "2",
+        caseUp: '"',
+        caseCaps: "2",
+        caseCapsShift: '"'
+      },
+      addClass: 'digit2'
+    },
+    {
+      name: 'digit3',
+      eng: {
+        caseDown: '3',
+        caseUp: '#',
+        caseCaps: '3',
+        caseCapsShift: '#'
+      },
+      ukr: {
+        caseDown: "3",
+        caseUp: '№',
+        caseCaps: "3",
+        caseCapsShift: '№'
+      },
+      addClass: 'digit3'
+    },
+    {
+      name: 'digit4',
+      eng: {
+        caseDown: '4',
+        caseUp: '$',
+        caseCaps: '4',
+        caseCapsShift: '$'
+      },
+      ukr: {
+        caseDown: "4",
+        caseUp: ';',
+        caseCaps: "4",
+        caseCapsShift: ';'
+      },
+      addClass: 'digit4'
+    },
+    {
+      name: 'digit5',
+      eng: {
+        caseDown: '5',
+        caseUp: '%',
+        caseCaps: '5',
+        caseCapsShift: '%'
+      },
+      ukr: {
+        caseDown: "5",
+        caseUp: '%',
+        caseCaps: "5",
+        caseCapsShift: '%'
+      },
+      addClass: 'digit5'
+    },
+    {
+      name: 'digit6',
+      eng: {
+        caseDown: '6',
+        caseUp: '^',
+        caseCaps: '6',
+        caseCapsShift: '^'
+      },
+      ukr: {
+        caseDown: "6",
+        caseUp: ':',
+        caseCaps: "6",
+        caseCapsShift: ':'
+      },
+      addClass: 'digit6'
+    },
+    {
+      name: 'digit7',
+      eng: {
+        caseDown: '7',
+        caseUp: '&',
+        caseCaps: '7',
+        caseCapsShift: '&'
+      },
+      ukr: {
+        caseDown: "7",
+        caseUp: '?',
+        caseCaps: "7",
+        caseCapsShift: '?'
+      },
+      addClass: 'digit7'
+    },
+    {
+      name: 'digit8',
+      eng: {
+        caseDown: '8',
+        caseUp: '*',
+        caseCaps: '8',
+        caseCapsShift: '*'
+      },
+      ukr: {
+        caseDown: "8",
+        caseUp: '*',
+        caseCaps: "8",
+        caseCapsShift: '*'
+      },
+      addClass: 'digit8'
+    },
+    {
+      name: 'digit9',
+      eng: {
+        caseDown: '9',
+        caseUp: '(',
+        caseCaps: '9',
+        caseCapsShift: '('
+      },
+      ukr: {
+        caseDown: "9",
+        caseUp: '(',
+        caseCaps: "9",
+        caseCapsShift: '('
+      },
+      addClass: 'digit9'
+    },
+    {
+      name: 'digit0',
+      eng: {
+        caseDown: '0',
+        caseUp: ')',
+        caseCaps: '0',
+        caseCapsShift: ')'
+      },
+      ukr: {
+        caseDown: "0",
+        caseUp: ')',
+        caseCaps: "0",
+        caseCapsShift: ')'
+      },
+      addClass: 'digit0'
+    },
+    {
+      name: 'minus',
+      eng: {
+        caseDown: '-',
+        caseUp: '_',
+        caseCaps: '-',
+        caseCapsShift: '_'
+      },
+      ukr: {
+        caseDown: "-",
+        caseUp: '_',
+        caseCaps: "-",
+        caseCapsShift: '_'
+      },
+      addClass: 'minus'
+    },
+    {
+      name: 'plus',
+      eng: {
+        caseDown: '=',
+        caseUp: '+',
+        caseCaps: '=',
+        caseCapsShift: '+'
+      },
+      ukr: {
+        caseDown: "=",
+        caseUp: '+',
+        caseCaps: "=",
+        caseCapsShift: '+'
+      },
+      addClass: 'plus'
+    },
+    {
+      name: 'backspace',
+      eng: {
+        caseDown: 'Backspace',
+        caseUp: 'Backspace',
+        caseCaps: 'Backspace',
+        caseCapsShift: 'Backspace'
+      },
+      ukr: {
+        caseDown: "Backspace",
+        caseUp: 'Backspace',
+        caseCaps: "Backspace",
+        caseCapsShift: 'Backspace'
+      },
+      addClass: 'backspace'
+    },
+
   ]
 
 ]
