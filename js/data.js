9f82cab1a62a27a6c617c386b0c14fec6a1525f5
Igor Kodzhebash <igorkodzebash@gmail.com>
2023-04-26 21:29:58 +0300

    feat: add third row for keyboard keys

--- js/data.js
+++ js/data.js
@@ -452,7 +452,217 @@ const data = [
       addClass: 'Backslash'
     },
 
-  ]
+  ],
+  [
+    {
+      name: 'CapsLock',
+      eng: {
+        caseDown: 'Caps Lock',
+        caseUp: 'Caps Lock',
+        caseCaps: 'Caps Lock',
+        caseCapsShift: 'Caps Lock'
+      },
+      ukr: {
+        caseDown: "Caps Lock",
+        caseUp: 'Caps Lock',
+        caseCaps: "Caps Lock",
+        caseCapsShift: 'Caps Lock'
+      },
+      addClass: 'CapsLock'
+    },
+    {
+      name: 'KeyA',
+      eng: {
+        caseDown: 'a',
+        caseUp: 'A',
+        caseCaps: 'a',
+        caseCapsShift: 'A'
+      },
+      ukr: {
+        caseDown: "ф",
+        caseUp: 'Ф',
+        caseCaps: "ф",
+        caseCapsShift: 'Ф'
+      },
+      addClass: 'KeyA'
+    },
+    {
+      name: 'KeyS',
+      eng: {
+        caseDown: 's',
+        caseUp: 'S',
+        caseCaps: 's',
+        caseCapsShift: 'S'
+      },
+      ukr: {
+        caseDown: "і",
+        caseUp: 'І',
+        caseCaps: "і",
+        caseCapsShift: 'І'
+      },
+      addClass: 'KeyS'
+    },
+    {
+      name: 'KeyD',
+      eng: {
+        caseDown: 'd',
+        caseUp: 'D',
+        caseCaps: 'd',
+        caseCapsShift: 'D'
+      },
+      ukr: {
+        caseDown: "в",
+        caseUp: 'В',
+        caseCaps: "в",
+        caseCapsShift: 'В'
+      },
+      addClass: 'KeyD'
+    },
+    {
+      name: 'KeyF',
+      eng: {
+        caseDown: 'f',
+        caseUp: 'F',
+        caseCaps: 'f',
+        caseCapsShift: 'F'
+      },
+      ukr: {
+        caseDown: "а",
+        caseUp: 'А',
+        caseCaps: "а",
+        caseCapsShift: 'А'
+      },
+      addClass: 'KeyF'
+    },
+    {
+      name: 'KeyG',
+      eng: {
+        caseDown: 'g',
+        caseUp: 'G',
+        caseCaps: 'g',
+        caseCapsShift: 'G'
+      },
+      ukr: {
+        caseDown: "п",
+        caseUp: 'П',
+        caseCaps: "п",
+        caseCapsShift: 'П'
+      },
+      addClass: 'KeyG'
+    },
+    {
+      name: 'KeyH',
+      eng: {
+        caseDown: 'h',
+        caseUp: 'H',
+        caseCaps: 'h',
+        caseCapsShift: 'H'
+      },
+      ukr: {
+        caseDown: "р",
+        caseUp: 'Р',
+        caseCaps: "р",
+        caseCapsShift: 'Р'
+      },
+      addClass: 'KeyH'
+    },
+    {
+      name: 'KeyJ',
+      eng: {
+        caseDown: 'j',
+        caseUp: 'J',
+        caseCaps: 'j',
+        caseCapsShift: 'J'
+      },
+      ukr: {
+        caseDown: "о",
+        caseUp: 'О',
+        caseCaps: "о",
+        caseCapsShift: 'О'
+      },
+      addClass: 'KeyJ'
+    },
+    {
+      name: 'KeyK',
+      eng: {
+        caseDown: 'k',
+        caseUp: 'K',
+        caseCaps: 'k',
+        caseCapsShift: 'K'
+      },
+      ukr: {
+        caseDown: "л",
+        caseUp: 'Л',
+        caseCaps: "л",
+        caseCapsShift: 'Л'
+      },
+      addClass: 'KeyK'
+    },
+    {
+      name: 'KeyL',
+      eng: {
+        caseDown: 'l',
+        caseUp: 'L',
+        caseCaps: 'l',
+        caseCapsShift: 'L'
+      },
+      ukr: {
+        caseDown: "д",
+        caseUp: 'Д',
+        caseCaps: "д",
+        caseCapsShift: 'Д'
+      },
+      addClass: 'KeyL'
+    },
+    {
+      name: 'Semicolon',
+      eng: {
+        caseDown: ';',
+        caseUp: ':',
+        caseCaps: ';',
+        caseCapsShift: ':'
+      },
+      ukr: {
+        caseDown: "ж",
+        caseUp: 'Ж',
+        caseCaps: "ж",
+        caseCapsShift: 'Ж'
+      },
+      addClass: 'Semicolon'
+    },
+    {
+      name: 'Quote',
+      eng: {
+        caseDown: `'`,
+        caseUp: `"`,
+        caseCaps: `'`,
+        caseCapsShift: `"`
+      },
+      ukr: {
+        caseDown: "є",
+        caseUp: 'Є',
+        caseCaps: "є",
+        caseCapsShift: 'Є'
+      },
+      addClass: 'Quote'
+    },
+    {
+      name: 'Enter',
+      eng: {
+        caseDown: 'Enter',
+        caseUp: 'Enter',
+        caseCaps: 'Enter',
+        caseCapsShift: 'Enter'
+      },
+      ukr: {
+        caseDown: "Enter",
+        caseUp: 'Enter',
+        caseCaps: "Enter",
+        caseCapsShift: 'Enter'
+      },
+      addClass: 'Enter'
+    },
+  ],
 
 ]
 
